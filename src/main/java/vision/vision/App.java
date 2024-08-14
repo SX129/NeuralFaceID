24437272b34a81fc4418ecc8beb4790ed11d331b
Henry Nguyen <nguyenhenry682@gmail.com>
2024-08-14 17:53:46 -0500

    added or, nor, and nand gate

--- src/main/java/vision/vision/App.java
+++ src/main/java/vision/vision/App.java
@@ -21,13 +21,28 @@ public class App
 		return neuron(new double[] {x1, x2}, new double[] {1, 1}, -1);
 	}
 	
+	// OR gate
+	static double or(double x1, double x2) {
+		return neuron(new double[] {x1, x2}, new double[] {1, 1}, 0);
+	}
+	
+	// NOR gate
+	static double nor(double x1, double x2) {
+		return neuron(new double[] {x1, x2}, new double[] {-1, -1}, 1);
+	}
+	
+	// NAND gate
+	static double nand(double x1, double x2) {
+		return neuron(new double[] {x1, x2}, new double[] {-1, -1}, 2);
+	}
+	
     public static void main( String[] args )
     {
     	for(int i = 0; i < 4; i++) {
     		double x1 = i / 2;
     		double x2 = i % 2;
     		
-    		double output = and(x1, x2);
+    		double output = nand(x1, x2);
     		
     		System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);
     	}
