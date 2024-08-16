9da3c2a8760521e5048a5161a0b7f2f0dc1a17ac
Henry Nguyen <nguyenhenry682@gmail.com>
2024-08-15 23:37:14 -0500

    add Matrix class for matrix operations and junit tests

---
+++ src/main/java/vision/matrix/Matrix.java
@@ -0,0 +1,93 @@
+package vision.matrix;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Matrix {
+	
+	private static final String NUMBER_FORMAT = "%+12.5f";
+	private static final double TOLERANCE = 0.000001;
+	
+	private int rows;
+	private int cols;
+	
+	public interface Producer {
+		double produce(int index);
+	}
+	
+	public interface ValueProducer {
+		double produce(int index, double value);
+	}
+	
+	private double[] a;
+	
+	public Matrix(int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
+		a = new double[rows * cols];
+	}
+	
+	public Matrix(int rows, int cols, Producer producer) {
+		this(rows, cols);
+		
+		for (int i = 0; i < a.length; i++) {
+			a[i] = producer.produce(i);
+		}
+	}
+	
+	public Matrix apply(ValueProducer producer) {
+		Matrix result = new Matrix(rows, cols);
+		
+		for (int i = 0; i < a.length; i++) {
+			result.a[i] = producer.produce(i, a[i]);
+		}
+		
+		return result;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(a);
+		result = prime * result + Objects.hash(cols, rows);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Matrix other = (Matrix) obj;
+		
+		for (int i = 0; i < a.length; i++) {
+			if (Math.abs(a[i] - other.a[i]) > TOLERANCE) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		int index = 0;
+		
+		for(int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
+				sb.append(String.format(NUMBER_FORMAT, a[index]));
+				
+				index++;
+			}
+			
+			sb.append("\n");
+		}
+		
+		return sb.toString();
+	}
+}
---
+++ src/test/java/matrix/MatrixTest.java
@@ -0,0 +1,70 @@
+package matrix;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import vision.matrix.Matrix;
+
+public class MatrixTest {
+	
+	@Test
+	public void testEquals() {
+		Matrix m1 = new Matrix(3, 4, i -> 0.5 * (i - 6));
+		Matrix m2 = new Matrix(3, 4, i -> 0.5 * (i - 6));
+		Matrix m3 = new Matrix(3, 4, i -> 0.5 * (i - 6.2));
+		
+		assertTrue(m1.equals(m2));
+		assertFalse(m1.equals(m3));
+	}
+	
+	@Test
+	public void testMultiplyDouble() {
+		Matrix m = new Matrix(3, 4, i -> 0.5 * (i - 6));
+		
+		double x = 0.5;
+		
+		Matrix result = m.apply((index, value)->x * value);
+		
+		System.out.println(m);
+		
+		System.out.println(result);
+	}
+	
+	@Test
+	public void testToString() {
+		Matrix m = new Matrix(3, 4, i->i*2);
+
+		String text = m.toString();
+		
+		double[] expected = new double[12];
+		
+		for (int i = 0; i < expected.length; i++) {
+			expected[i] = i * 2;
+		}
+		
+		var rows = text.split("\n");
+		
+		assertTrue(rows.length == 3);
+		
+		int index = 0;
+		
+		for(var row : rows) {
+			var values = row.split("\\s+");
+			
+			for(var textValue : values) {
+				
+				if (textValue.length() == 0) {
+					continue;
+				}
+
+				var doubleValue = Double.valueOf(textValue);
+				
+				assertTrue(Math.abs(doubleValue - expected[index]) < 0.00001);
+				
+				index++;
+			}
+		}
+	}
+
+}
