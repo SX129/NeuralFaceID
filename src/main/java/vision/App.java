75ce004f0957bed29b8d39ec65de4323f5e08e34
Henry Nguyen <nguyenhenry682@gmail.com>
2024-08-25 14:36:49 -0500

    update neural network with learning rate and epochs

--- src/main/java/vision/App.java
+++ src/main/java/vision/App.java
@@ -2,6 +2,8 @@ package vision;
 
 import vision.neuralnetwork.NeuralNetwork;
 import vision.neuralnetwork.Transform;
+import vision.neuralnetwork.loader.Loader;
+import vision.neuralnetwork.loader.test.TestLoader;
 
 public class App {
 	public static void main(String[] args) {
@@ -14,6 +16,14 @@ public class App {
 		neuralNetwork.add(Transform.RELU);
 		neuralNetwork.add(Transform.DENSE, outputRows);
 		neuralNetwork.add(Transform.SOFTMAX);
+		
+		neuralNetwork.setEpochs(20);
+		neuralNetwork.setLearningRate(0.02, 0);
+		
+		Loader trainLoader = new TestLoader(60_000, 32);
+		Loader testLoader = new TestLoader(10_000, 32);
+		
+		neuralNetwork.fit(trainLoader, testLoader);
 		
 		System.out.println(neuralNetwork);
 	}
--- src/main/java/vision/neuralnetwork/NeuralNetwork.java
+++ src/main/java/vision/neuralnetwork/NeuralNetwork.java
@@ -1,7 +1,14 @@
 package vision.neuralnetwork;
 
+import vision.neuralnetwork.loader.Loader;
+
 public class NeuralNetwork {
 	private Engine engine;
+	
+	private int epochs = 20;
+	private double learningRate;
+	private double initialLearningRate = 0.01;
+	private double finalLearningRate = 0;
 	
 	public NeuralNetwork() {
 		engine = new Engine();
@@ -9,6 +16,34 @@ public class NeuralNetwork {
 	
 	public void add(Transform transform, double... params) {
 		engine.add(transform, params);
+	}
+	
+	public void setLearningRate(double initailLearningRate, double finalLearningRate) {
+		this.initialLearningRate = initailLearningRate;
+		this.finalLearningRate = finalLearningRate;
+	}
+	
+	public void setEpochs(int epochs) {
+		this.epochs = epochs;
+	}
+	
+	public void fit(Loader trainLoader, Loader evalLoader) {
+		learningRate = initialLearningRate;
+		
+		for(int epoch = 0; epoch < epochs; epoch++) {
+			System.out.printf("Epoch %3d ", epoch);
+			runEpoch(trainLoader, true);
+			
+			if(evalLoader != null) {
+				runEpoch(evalLoader, false);
+			}
+			
+			learningRate -= (initialLearningRate - finalLearningRate) / epochs;
+		}
+	}
+
+	private void runEpoch(Loader loader, boolean trainingMode) {
+		
 	}
 
 	@Override
