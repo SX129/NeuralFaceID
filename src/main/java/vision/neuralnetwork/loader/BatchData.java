eb2df11f2f8fa119283053fb74bde6af1430fd39
Henry Nguyen <nguyenhenry682@gmail.com>
2024-08-25 13:44:53 -0500

    created batchData interface

---
+++ src/main/java/vision/neuralnetwork/loader/BatchData.java
@@ -0,0 +1,11 @@
+package vision.neuralnetwork.loader;
+
+public interface BatchData {
+	public double[] getInputBatch();
+
+	public void setInputBatch(double[] inputBatch);
+
+	public double[] getExpectedBatch();
+
+	public void setExpectedBatch(double[] expectedBatch);
+}
